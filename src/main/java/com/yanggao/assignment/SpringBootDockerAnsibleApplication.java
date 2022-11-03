a794d73f246617fb42e7183927f02bfd47287db8
Yang Gao <ygaowhu@outlook.com>
2022-11-03 09:38:09 -0400

    Initial commit

---
+++ src/main/java/com/yanggao/assignment/SpringBootDockerAnsibleApplication.java
@@ -0,0 +1,25 @@
+package com.yanggao.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;lication;
+
+@SpringBootApplication
+public class SpringBootDockerAnsibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerAnsibleApplication.class, args);
+	}
+
+	@RestController
+	static class BootController {
+		@GetMapping("/{name}")
+		public ResponseEntity<String> greeting(@PathVariable(value = "name") String name) {
+			return ResponseEntity.ok("Hi " + name + ", how are you?");
+		}
+	}
+
+}
---
+++ src/test/java/com/yanggao/assignment/SpringBootDockerAnsibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanggao.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerAnsibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
